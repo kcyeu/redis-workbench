ef2d42a177100e021617d875941c9369b671aaa0
Gordon Yeu <kmd@mikuru.tw>
2015-02-15 17:14:11 +0800

    added docker support

--- command.php
+++ command.php
@@ -5,6 +5,11 @@ require_once __DIR__ . '/error_reporting.php';
 define('REDIS_CLI_CMD', '/usr/local/bin/redis-cli');
 $redis_cli_cmd = REDIS_CLI_CMD;
 
+if ( ! $redis_address = getenv('REDIS_PORT_6379_TCP_ADDR'))
+{
+    $redis_address = "127.0.0.1";    
+}
+
 header('Content-type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] != 'POST')
