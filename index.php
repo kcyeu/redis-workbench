9c71129691c13785a57fe50d6c5bab353cc8f389
Gordon Yeu <kmd@mikuru.tw>
2015-02-19 20:57:09 +0800

    added [SERVER_ADDR]

--- index.php
+++ index.php
@@ -102,5 +102,6 @@ KEYS *
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
   <script src="./js/main.js"></script>
+  <!-- <?php echo "Server address: " . $_SERVER["SERVER_ADDR"]; ?> -->
 </body>
 </html>
