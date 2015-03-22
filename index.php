eb6dc1a56a406018badbf3b70b22790b29c7aa84
Gordon Yeu <kmd@mikuru.tw>
2015-03-22 02:05:55 -0700

    added BootSwatch theme demo

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <title>Redis Workbench</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" href="./css/styles.css">
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+  <!--
+  <link rel="stylesheet" class="bootswatcher" href="http://bootswatch.com/superhero/bootstrap.min.css">  
+  -->
 </head>
 <body>
   <div class="container main">
@@ -100,7 +103,7 @@ KEYS *
     </div>
   </footer>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script src="./js/main.js"></script>
   <!-- <?php echo "Server address: " . $_SERVER["SERVER_ADDR"]; ?> -->
 </body>
