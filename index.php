11465fe3ccf5851d4215fe6db7ad258eabdb5ad0
Gordon Yeu <kmd@mikuru.tw>
2015-03-22 02:08:59 -0700

    enabled theme

--- index.php
+++ index.php
@@ -11,9 +11,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <!--
   <link rel="stylesheet" class="bootswatcher" href="http://bootswatch.com/superhero/bootstrap.min.css">  
-  -->
 </head>
 <body>
   <div class="container main">
